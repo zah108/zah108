1fcef9fb190c89c2bb34a9e45f0c9af0ea4d400d
zah108 <152983743+zah108@users.noreply.github.com>
2024-01-04 16:17:52 +0300

    Add files

--- A-Trade/plotly-1.py
+++ A-Trade/plotly-1.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df = pd.read_csv('Data/AAPL.csv')
 
 fig = go.Figure(data=[go.Candlestick(x=df['Date'],
                 open=df['AAPL.Open'],
@@ -12,20 +12,16 @@ fig = go.Figure(data=[go.Candlestick(x=df['Date'],
                 low=df['AAPL.Low'],
                 close=df['AAPL.Close'])])
 
+fig.update_layout(
+    title='AAPL',
+    xaxis_title='Date',
+    # shapes = [dict(
+    #     x0='1970', x1='2015', y0=135, y1=135, xref='y', yref='paper',
+    #     line_width=2)],
+    # annotations=[dict(
+    #     x='2016-12-09', y=0.05, xref='x', yref='paper',
+    #     showarrow=False, xanchor='left', text='Increase Period Begins')]
+)
+
 fig.show()
 
-# import pandas as pd
-
-# df = pd.DataFrame({
-#   "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-#   "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
-#   "Number Eaten": [2, 1, 3, 1, 3, 2],
-# })
-
-
-# # Plotly Express
-
-# import plotly.express as px
-
-# fig = px.bar(df, x="Fruit", y="Number Eaten", color="Contestant", barmode="group")
-# fig.show()
