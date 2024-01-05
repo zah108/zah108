abf154a2375c7f30ef600d39734950e37c44daef
zah108 <152983743+zah108@users.noreply.github.com>
2024-01-05 14:59:18 +0300

    Version 2

--- A-Trade/plotly-1.py
+++ A-Trade/plotly-1.py
@@ -1,4 +1,8 @@
-
+'''
+Код исправлен под автоматически получаемые исторические данные с МОЕХ
+Исправлены названия столбцов.
+Графика - библиотека Plotly. Строит в браузуре http://127.0.0.1:53196/ 
+'''
 import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime
@@ -6,21 +10,21 @@ from datetime import datetime
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 df = pd.read_csv('Data/SBER_D1.csv')
 
-# fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-#                 open=df['AAPL.Open'],
-#                 high=df['AAPL.High'],
-#                 low=df['AAPL.Low'],
-#                 close=df['AAPL.Close'])])
-
 fig = go.Figure(data=[go.Candlestick(x=df['datetime'],
                 open=df['open'],
                 high=df['high'],
                 low=df['low'],
                 close=df['close'])])
 
+# fig = go.Figure(data=[go.Candlestick(x=df['datetime'],
+#                 open=df['open'],
+#                 high=df['high'],
+#                 low=df['low'],
+#                 close=df['close'])])
+
 
 fig.update_layout(
-    title='AAPL',
+    title='SBER',
     xaxis_title='Date',
     # shapes = [dict(
     #     x0='1970', x1='2015', y0=135, y1=135, xref='y', yref='paper',
